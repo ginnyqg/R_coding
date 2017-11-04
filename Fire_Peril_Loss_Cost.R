4881418841b8d603a8fb255a4c7bc87ae49290a7
ginnyqg <21312923+ginnyqg@users.noreply.github.com>
2017-11-04 15:38:18 -0400

    Create Fire_Peril_Loss_Cost.R

---
+++ Fire_Peril_Loss_Cost.R
@@ -0,0 +1,27 @@
+#Import, EDA, train
+
+library(data.table)
+
+#import train data
+fire_train <- fread('Fire Peril Loss Cost/train.csv')
+
+#dimension: number of rows and columns
+dim(fire_train)
+
+#peek at first few rows
+head(fire_train)
+
+#overview of variables
+summary(fire_train)
+
+
+
+#EDA target, exclude target is 0
+hist(fire_train$target[! fire_train$target == 0])
+
+
+
+
+
+
+
