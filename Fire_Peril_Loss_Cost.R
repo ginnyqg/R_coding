c623f19a67f5f4d5ef4c10b16a0b665677a77cf0
ginnyqg <21312923+ginnyqg@users.noreply.github.com>
2017-11-07 22:01:14 -0500

    Update Fire_Peril_Loss_Cost.R

--- Fire_Peril_Loss_Cost.R
+++ Fire_Peril_Loss_Cost.R
@@ -25,9 +25,14 @@ barplot(table(fire_train $var1))
 #count number of NAs in continuous column
 sum(is.na(fire_train$var12))
 
-#impute NA to 0
+#impute NA in var12, 14, 15, 16 to 0
 fire_train$var12[is.na(fire_train$var12)] = 0
 
 
+#transform datset to data frame
+df_raw <- data.frame(fire_train)
+
+#extract useful columns
+df_train <- df_raw[, c(2, 12:19)]
 
 
