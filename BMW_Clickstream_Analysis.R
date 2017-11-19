e0e46d3289ddb72a22680596eae612aa47a91778
ginnyqg <21312923+ginnyqg@users.noreply.github.com>
2017-11-19 16:17:40 -0500

    Create BMW_Clickstream_Analysis.R

---
+++ BMW_Clickstream_Analysis.R
@@ -0,0 +1,8 @@
+library(data.table)
+
+Oct_2_2017 <- fread('/Users/qinqingao/Desktop/Columbia/Contest/Data/Oct2 sample.csv')
+#Read 21258 rows and 503 (of 503) columns from 0.117 GB file in 00:00:03
+
+View(Oct_2_2017)
+
+
