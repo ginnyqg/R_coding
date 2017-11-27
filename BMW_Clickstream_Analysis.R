69e67af7d2b2d60dbac6e9ff99d1a030c75bd107
ginnyqg <21312923+ginnyqg@users.noreply.github.com>
2017-11-27 15:53:10 -0500

    Update BMW_Clickstream_Analysis.R

--- BMW_Clickstream_Analysis.R
+++ BMW_Clickstream_Analysis.R
@@ -6,3 +6,22 @@ Oct_2_2017 <- fread('/Users/qinqingao/Desktop/Columbia/Contest/Data/Oct2 sample.
 View(Oct_2_2017)
 
 
+
+
+
+
+
+
+#terminal commands
+cd /Users/qinqingao
+
+ssh qg2158@12.42.205.8
+
+mysql -u qg2158 -p
+
+use qg2158
+
+show tables;
+
+
+
