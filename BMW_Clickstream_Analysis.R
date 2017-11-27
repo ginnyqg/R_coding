728601465601c8c8002c79bf4290fd5ffa2652e4
ginnyqg <21312923+ginnyqg@users.noreply.github.com>
2017-11-27 16:01:16 -0500

    Update BMW_Clickstream_Analysis.R

--- BMW_Clickstream_Analysis.R
+++ BMW_Clickstream_Analysis.R
@@ -1,27 +1,10 @@
 library(data.table)
 
-Oct_2_2017 <- fread('/Users/qinqingao/Desktop/Columbia/Contest/Data/Oct2 sample.csv')
+Oct_2_2017 <- fread('Oct2 sample.csv')
 #Read 21258 rows and 503 (of 503) columns from 0.117 GB file in 00:00:03
 
 View(Oct_2_2017)
 
-
-
-
-
-
-
-
-#terminal commands
-cd /Users/qinqingao
-
-ssh qg2158@12.42.205.8
-
-mysql -u qg2158 -p
-
-use qg2158
-
-show tables;
 
 
 
