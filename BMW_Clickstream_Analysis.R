3d21175b05525f6e00ad83fe8ddfcccaae0cd14e
ginnyqg <21312923+ginnyqg@users.noreply.github.com>
2017-11-30 00:54:36 -0500

    Update BMW_Clickstream_Analysis.R

--- BMW_Clickstream_Analysis.R
+++ BMW_Clickstream_Analysis.R
@@ -8,18 +8,33 @@ View(Oct_2_2017)
 dim(Oct_2_2017)
 #[1] 21258   503
 
-#Exclude all rows where exclude_hit > 0
-nrow(Oct_2_2017[Oct_2_2017$exclude_hit > 0, ])
-#[1] 117
+#Data selection
 
-#Exclude all rows where hit_source = 5,7,8,9
-nrow(Oct_2_2017[Oct_2_2017$hit_source %in% c(5,7,8,9), ])
-#[1] 1
+#Select post_visid_type value of 0,1,2,3,4,5
+selected_data <- filter(df, df$post_visid_type %in% c(0,1,2,3,4,5))
 
+dim(selected_data)
+#[1] 11823   503
 
+df <- selected_data
 
+dim(df)
+#[1] 11823   503
 
+selected_data <- filter(df, df$exclude_hit == 0)
 
+dim(selected_data)
+#[1] 11761   503
+
+df <- selected_data
+
+dim(df)
+#[1] 11761   503
+
+selected_data <- filter(df, !(df$hit_source %in% c(5,7,8,9)))
+
+dim(selected_data)
+#[1] 11761   503
 
 
 
